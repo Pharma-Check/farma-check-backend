22df554ced05af910d0afc6e2a5e5623656ca606
Pharma-Check <renatoalmeidafranca@hotmail.com>
2026-09-20 14:22:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { createClient } = require('@supabase/supabase-client');
+const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 const port = process.env.PORT || 3000;
